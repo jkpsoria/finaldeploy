b4cac0e08c3b5a91179bd4c984e6dcb23a717e97
stambalerdy <jkpsoria@gmail.com>
2022-04-05 23:16:04 +0800

    added the files

---
+++ conferenceroom/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import *
+
+class RoomForm(forms.ModelForm):
+	class Meta:
+		model = Room
+		fields= '__all__'
+class RoomReservationForm(forms.ModelForm):
+	class Meta:
+		model = RoomReservation
+		fields= '__all__'
+class AddRoomForm(forms.ModelForm):
+	class Meta:
+		model = AddRoom
+		fields= '__all__'
+class UserForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields= '__all__'
